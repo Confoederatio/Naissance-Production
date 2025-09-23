5cb3f2eaec454d443163fd76722df3e2059f9366
Confoederatio <htmlprogrammingbn@gmail.com>
2025-09-23 14:03:04 +0100

    added ComponentNumber for testing purposes. adding strings next

--- UF/js/vercengen/classes/VercengenClass.js
+++ UF/js/vercengen/classes/VercengenClass.js
@@ -33,8 +33,25 @@ ve.Class = class {
 		//Return statement
 	}
 	
-	open (arg0_mode, arg1_options) {
-	
+	/**
+	 * Opens the relevant window
+	 * @param {string} [arg0_mode="instance"] - Whether the UI is bound to 'class'/'instance'. If 'class', it displays all static Vercengen fields.
+	 * @param {Object} [arg1_options]
+	 *  @param {boolean} [arg1_options.do_not_close_if_open=false] - Whether to close the Window already bound to class_window or instance_window
+	 *  @param {boolean} [arg1_options.is_window=false] - If the item is not a Window, then it is a frozen form of a Window with a constant z-index that cannot be moved/closed
+	 *
+	 *  @param {string} [arg1_options.anchor="top_left"] - Either 'bottom_left'/'bottom_right'/'top_left'/'top_right'. If neither this nor .x/.y are defined, the UI is spawned at the cursor position
+	 *  @param {number|string} [arg1_options.height]
+	 *  @param {number|string} [arg1_options.width]
+	 *  @param {number|string} [arg1_options.x] - Mouse coordinates if undefined
+	 *  @param {number|string} [arg1_options.y] - Mouse coordinates if undefined
+	 */
+	open (arg0_mode, arg1_options) { //[WIP] - Finish function body
+		//Convert from parameters
+		let mode = (arg0_mode) ? arg0_mode : "instance";
+		let options = (arg1_options) ? arg1_options : {};
+		
+		//Declare local instance variables
 	}
 	
 	//State methods
---
+++ UF/js/vercengen/components/ComponentNumber.js
@@ -0,0 +1,52 @@
+ve.Number = class {
+	constructor (arg0_value, arg1_options) {
+		//Convert from parameters
+		let value = arg0_value;
+		let options = (arg1_options) ? arg1_options : {};
+		
+		//Declare local instance variables
+		let html_string = [];
+		this.element = document.createElement("span");
+		this.value = value;
+		
+		if (options.name) html_string.push(`<span>${options.name}</span> `);
+		html_string.push(`<input type = "number">`);
+		
+		//Populate element and initialise handlers
+		this.element.innerHTML = html_string.join("");
+			this.element.querySelector("input").addEventListener("input", (e) => {
+				this.value = Number(e.target.value);
+			});
+	}
+	
+	bindTo (arg0_container_el) {
+		//Convert from parameters
+		let container_el = arg0_container_el;
+		
+		//Set variable_key, append to container_el
+		container_el.appendChild(this.element);
+	}
+	
+	get () {
+		//Return statement
+		return this.value;
+	}
+	
+	set (arg0_value) {
+		//Convert from parameters
+		let value = arg0_value;
+		
+		//Set value and update UI
+		this.value = value;
+		this.element.querySelector("input").value = this.value;
+	}
+	
+	//Class methods
+	toString () {
+		return String(this.value);
+	}
+	
+	valueOf () {
+		return this.value;
+	}
+};
