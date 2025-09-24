6dfbe2b72ffab82e243a3a0e58a2ad11ef03bc7b
Confoederatio <htmlprogrammingbn@gmail.com>
2025-09-24 23:43:06 +0100

    removed archives from checking

--- UF/js/vercengen/windows/Window.js
+++ UF/js/vercengen/windows/Window.js
@@ -1,3 +1,10 @@
 ve.Window = class {
-
+	constructor (arg0_components_obj, arg1_options) {
+		//Convert from parameters
+		let components_obj = arg0_components_obj;
+		let options = (arg1_options) ? arg1_options : {};
+		
+		//Declare local instance variables
+		
+	}
 };
