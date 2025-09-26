ba0ef39b2cd0dd30581ca13ce0a9ca3d9a715781
Confoederatio <htmlprogrammingbn@gmail.com>
2025-09-26 18:05:32 +0100

    finished component colour

---
+++ UF/js/vercengen/components/ComponentColour.js
@@ -0,0 +1,70 @@
+setTimeout(() => {
+	ve.Colour = class veColour extends ve.Component {
+		constructor (arg0_value, arg1_options) {
+			//Convert from parameters
+			let value = arg0_value;
+			let options = (arg1_options) ? arg1_options : {};
+			super(options);
+			
+			//Initialise options
+			options.attributes = options.attributes ? options.attributes : {};
+			
+			//Declare local instance variables
+			this.element = document.createElement("span");
+			this.element.setAttribute("component", "ve-colour");
+			this.element.instance = this;
+			HTML.applyCSSStyle(this.element, options.style);
+			
+			this.value = value;
+			
+			//Format HTML string
+			let html_string = [];
+			if (options.name) html_string.push(`<span>${options.name}</span> `);
+			html_string.push(`<input type = "color"${HTML.objectToAttributes(options.attributes)}>`);
+			
+			//Populate element and initialise handlers; set .instance
+			this.element.innerHTML = html_string.join("");
+			
+			let input_el = this.element.querySelector("input");
+			input_el.addEventListener("input", (e) => {
+				this.value = e.target.value;
+			});
+			this.v = this.value;
+		}
+		
+		bind (arg0_container_el) {
+			//Convert from parameters
+			let container_el = arg0_container_el;
+			
+			//Set variable_key, append to container_el
+			container_el.appendChild(this.element);
+		}
+		
+		get v () {
+			//Return statement
+			return Colour.convertHexToRGBA(this.value);
+		}
+		
+		set v (arg0_value) {
+			//Convert from parameters
+			let value = Colour.convertRGBAToHex(arg0_value);
+			
+			//Set value and update UI
+			this.value = value;
+			this.element.querySelector("input").value = this.value;
+		}
+		
+		remove () {
+			this.element.remove();
+		}
+		
+		//Class methods
+		toString () {
+			return String(this.value);
+		}
+		
+		valueOf () {
+			return this.value;
+		}
+	};
+}, 0);
--- core/ui/test.js
+++ core/ui/test.js
@@ -6,7 +6,8 @@ setTimeout(() => {
 			this.interface = new ve.Interface({
 				test: new ve.Number(5, { name: "Test Number" }),
 				more_interface: new ve.Interface({
-					help: new ve.Number(1)
+					help: new ve.Number(1),
+					colour: new ve.Colour([255, 255, 255], { name: "Colour" })
 				}, { name: "More UI" })
 			}, { open: true })
 			super.open("instance", { name: "Help" });
