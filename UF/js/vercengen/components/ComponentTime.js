8a5e259796b1e2c60c45c6462aba2f616837ae15
Confoederatio <htmlprogrammingbn@gmail.com>
2025-10-01 07:08:00 +0100

    added time component

--- UF/js/vercengen/components/ComponentTime.js
+++ UF/js/vercengen/components/ComponentTime.js
@@ -1,12 +1,13 @@
-ve.Number = class veNumber extends ve.Component {
-	static demo_value = 1;
-	
+ve.Time = class veTime extends ve.Component {
 	constructor (arg0_value, arg1_options) {
 		//Convert from parameters
-		let value = arg0_value;
+		let value = (arg0_value !== undefined) ? arg0_value : {
+			hour: 0,
+			minute: 0
+		};
 		let options = (arg1_options) ? arg1_options : {};
 			super(options);
-		
+			
 		//Initialise options
 		options.attributes = (options.attributes) ? options.attributes : {};
 		
@@ -15,11 +16,10 @@ ve.Number = class veNumber extends ve.Component {
 			readonly: options.disabled,
 			max: options.max,
 			min: options.min,
-			step: options.step,
 			...options.attributes
 		};
 		this.element = document.createElement("div");
-			this.element.setAttribute("component", "ve-number");
+			this.element.setAttribute("component", "ve-time");
 			this.element.instance = this;
 		HTML.applyCSSStyle(this.element, options.style);
 		
@@ -28,21 +28,29 @@ ve.Number = class veNumber extends ve.Component {
 		//Format html_string
 		let html_string = [];
 		if (options.name) html_string.push(`<span>${options.name}</span> `);
-		html_string.push(`<input type = "number"${HTML.objectToAttributes(attributes)}>`);
+		html_string.push(`<input type = "time"${HTML.objectToAttributes(attributes)}>`);
 		
 		//Populate element and initialise handlers
 		this.element.innerHTML = html_string.join("");
 		
 		let input_el = this.element.querySelector("input");
 		input_el.addEventListener("input", (e) => {
-			this.v = global.Number(e.target.value);
+			let split_value = e.target.value.split(":");
+			
+			this.v = { hour: parseInt(split_value[0]), minute: parseInt(split_value[1]) };
 		});
 		this.v = this.value;
 	}
 	
 	get v () {
+		//Declare local instance variables
+		let split_value = this.element.querySelector("input").value.split(":");
+		
 		//Return statement
-		return this.value;
+		return {
+			hour: parseInt(split_value[0]),
+			minute: parseInt(split_value[1])
+		};
 	}
 	
 	set v (arg0_value) {
@@ -51,20 +59,12 @@ ve.Number = class veNumber extends ve.Component {
 		
 		//Set value and update UI
 		this.value = value;
-		this.element.querySelector("input").value = this.value;
+		console.log(value);
+		this.element.querySelector("input").value = `${value.hour.toString().padStart(2, "0")}:${value.minute.toString().padStart(2, "0")}`;
 		if (this.options.onchange) this.options.onchange(this.value);
 	}
 	
 	remove () {
 		this.element.remove();
-	}
-	
-	//Class methods
-	toString () {
-		return String(this.value);
-	}
-	
-	valueOf () {
-		return this.value;
 	}
 };
