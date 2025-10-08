ea4165faf96300dd8f80f007d238d7ee4284344d
Confoederatio <htmlprogrammingbn@gmail.com>
2025-10-08 14:05:15 +0100

    finished ve.PageMenu component

--- UF/js/vercengen/components/ComponentPageMenu.js
+++ UF/js/vercengen/components/ComponentPageMenu.js
@@ -1,5 +1,16 @@
 
 ve.PageMenu = class vePageMenu extends ve.Component {
+	static demo_value = {
+		page_one: {
+			name: "Home",
+			components_obj: {}
+		},
+		page_two: {
+			name: "About",
+			components_obj: {}
+		}
+	};
+	
 	constructor (arg0_page_obj, arg1_options) {
 		//Convert from parameters
 		let page_obj = arg0_page_obj;
@@ -7,54 +18,93 @@ ve.PageMenu = class vePageMenu extends ve.Component {
 			super(options);
 			
 		//Initialise options
+		options.attributes = (options.attributes) ? options.attributes : {};
 		options.starting_page = (options.starting_page) ? options.starting_page : Object.keys(page_obj)[0];
 		
 		//Declare local instance variables
 		this.element = document.createElement("div");
 			this.element.setAttribute("component", "ve-page-menu");
+			Object.iterate(options.attributes, (local_key, local_value) => {
+				this.element.setAttribute(local_key, local_value.toString());
+			});
 			this.element.instance = this;
 			HTML.applyCSSStyle(this.element, options.style);
 		this.interfaces_obj = {};
 		this.navbar_el = document.createElement("nav");
 			this.navbar_el.classList.add("navbar");
 		
-		//Append header element; navbar_el
-		//Iterate over all keys in page_obj and create ve.Interface instances for them
-		Object.iterate(page_obj, (local_key, local_value) => {
-			let local_name = (local_value.name) ? local_value.name : local_key;
-			let local_name_el = document.createElement("div");
+		//1. Navbar handling
+		{
+			//Append header element; navbar_el
+			//Iterate over all keys in page_obj and create ve.Interface; tab instances for them
+			Object.iterate(page_obj, (local_key, local_value) => {
+				let local_name = (local_value.name) ? local_value.name : local_key;
+				let local_name_el = document.createElement("div");
 				local_name_el.classList.add("tab");
 				if (local_key === options.starting_page)
 					local_name_el.classList.add("active");
+				local_name_el.id = local_key;
 				local_name_el.innerHTML = local_name;
-			
-			//Format navbar_el; populate this.interfaces_obj
-			this.navbar_el.appendChild(local_name_el);
-			this.interfaces_obj[local_key] = new ve.Interface(local_value.components_obj, local_value.options);
-		});
-		this.underline_el = document.createElement("span");
+				
+				//Format navbar_el; populate this.interfaces_obj
+				this.navbar_el.appendChild(local_name_el);
+				this.interfaces_obj[local_key] = new ve.Interface(local_value.components_obj, local_value.options);
+			});
+			this.underline_el = document.createElement("span");
 			this.underline_el.classList.add("underline");
 			this.navbar_el.appendChild(this.underline_el);
 			
-		//Append body element; interface_el
-		this.interface_el = document.createElement("div");
-		this.interface_el.id = "component-body";
+			//Append body element; interface_el
+			this.interface_el = document.createElement("div");
+			this.interface_el.id = "component-body";
 			
-		this.element.append(this.navbar_el, this.interface_el);
+			this.element.append(this.navbar_el, this.interface_el);
+			
+			//Add tab onclick handlers for this.navbar_el
+			let all_tabs = this.navbar_el.querySelectorAll(`.tab`);
+			
+			all_tabs.forEach((local_tab) => {
+				local_tab.addEventListener("click", () => {
+					all_tabs.forEach((local_tab) => local_tab.classList.remove("active"));
+					local_tab.classList.add("active");
+					this.updateUnderline();
+				});
+			});
+		}
 		
-		//Add tab onclick handlers for this.navbar_el
+		//2. Body handling; display starting interface
+		{
+			this.v = options.starting_page;
+		}
+	}
+	
+	get v () {
+		//Return statement
+		return this.navbar_el.querySelector(`.tab.active`).id;
+	}
+	
+	set v (arg0_page_key) {
+		//Convert from parameters
+		let page_key = arg0_page_key;
+		
+		//Declare local instance variables
+		let active_tab_el = this.navbar_el.querySelector(`.tab[id="${page_key}"]`);
 		let all_tabs = this.navbar_el.querySelectorAll(`.tab`);
 		
-		all_tabs.forEach((local_tab) => {
-			local_tab.addEventListener("click", () => {
-				all_tabs.forEach((local_tab) => local_tab.classList.remove("active"));
-				local_tab.classList.add("active");
-				this.updateUnderline();
-			});
-		});
+		//Modify active tab
+		all_tabs.forEach((local_tab) => local_tab.classList.remove("active"));
+		if (active_tab_el) {
+			active_tab_el.classList.add("active");
+		} else {
+			console.error(`active_tab_el could not be found for ${page_key}.`);
+		}
 		
-		//Initialise underline on load
-		this.updateUnderline();
+		//Switch interface to selected page
+		this.interface_el.innerHTML = "";
+		this.interface_el.appendChild(this.interfaces_obj[page_key].element);
+		setTimeout(() => {
+			this.updateUnderline();
+		}, 100);
 	}
 	
 	updateUnderline (arg0_animate_y) {
