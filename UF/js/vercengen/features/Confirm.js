77e831415ee9b0d84424ce6250a4a9e465fa8cb3
Confoederatio <htmlprogrammingbn@gmail.com>
2025-10-29 00:33:36 +0000

    finished documenting all ve.Feature classes

--- UF/js/vercengen/features/Confirm.js
+++ UF/js/vercengen/features/Confirm.js
@@ -2,16 +2,18 @@
  * Refer to <span color = "yellow">{@link ve.Feature}</span> for methods or fields inherited from the parent, such as automatic destructuring.
  * 
  * Confirmation prompt used for executing a `.options.special_function` call.
+ * - Functional binding: <span color=00ffff>veConfirm</span>().
  * - Inherits Feature: {@link ve.Modal}
  * 
  * ##### Constructor:
- * - `arg0_components_obj`: {@link function}|{@link Object}<{@link ve.Component}>|{@link string}
- * - `arg1_options`: {@link Object} - Same `.options` field as {@link ve.Modal}
+ * - `arg0_components_obj`: {@link function}|{@link Object}<{@link ve.Component}>|{@link string}|{@link ve.Component}
+ * - `arg1_options`: {@link Object} - Superset of `.options` for {@link ve.Modal}.
  *   - `.special_function`: {@link function}(v:{@link function}, e:{@link ve.Button})
  * 
  * ##### Methods::
  * - <span color=00ffff>{@link ve.Confirm.close|close}</span>()
  * 
+ * @augments {@link ve.Feature}
  * @type {ve.Confirm}
  */
 ve.Confirm = class extends ve.Feature {
