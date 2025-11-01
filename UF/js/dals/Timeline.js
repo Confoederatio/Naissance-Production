120692fad6d1eb0086eab9600adbf964e8ba5c69
Confoederatio <htmlprogrammingbn@gmail.com>
2025-11-01 07:19:52 +0000

    Update Timeline.js

--- UF/js/dals/Timeline.js
+++ UF/js/dals/Timeline.js
@@ -20,7 +20,7 @@ Object.defineProperty(DALS, "timeline", {
 	}
 });
 
-//[WIP] - Place DALS.Timeline on a JSON footing
+//[WIP] - Finished most scaffolding, refer to AI for logic errors/double checking
 DALS.Timeline = class {
 	//Declare local static variables
 	static current_index = 0;
@@ -112,16 +112,30 @@ DALS.Timeline = class {
 	 */
 	jumpToAction (arg0_action_id) {
 		//Convert from parameters
+		let action_id = arg0_action_id;
 		
 		//1. Cast index to action_id if typeof number, assuming that it is valid
+		if (typeof action_id === "number")
+			if (action_id <= this.value.length - 1)
+				action_id = this.value[action_id].id;
 		
 		//2. Load initial state at head
+		this.jumpToStart();
 		
 		//3. Redo actions starting from the state head using DALS.Timeline.parseAction() until we hit the target action ID
+		for (let i = 1; i < this.value.length; i++) {
+			DALS.Timeline.parseAction(this.value[i].value);
+			if (this.value[i].id === action_id) break;
+		}
 	}
 	
 	jumpToEnd () {
-		
+		//Jump to action if there are actions to jump to, otherwise load state head
+		if (this.value.length > 1) {
+			this.jumpToAction(this.value[this.value.length - 1].id);
+		} else {
+			this.jumpToStart();
+		}
 	}
 	
 	jumpToStart () {
@@ -131,10 +145,36 @@ DALS.Timeline = class {
 	
 	removeAction (arg0_action_id) {
 		//Convert from parameters
+		let action_id = arg0_action_id;
 		
-		//1. Go over all DALS.Timeline instances that branch from this timeline at an index greater or equal to the action being removed and set their new .parent_timeline to the end of the present timeline
+		//Declare local instance variables
+		let action_index = -1;
 		
-		//2. Splice all actions at and after the index from the current timeline
+		//1. Cast action_id to index, assuming that it is valid
+		if (typeof action_id === "string") {
+			//Iterate over all actions in this.value
+			for (let i = 1; i < this.value.length; i++)
+				if (this.value[i].id === action_id) {
+					action_index = i;
+					break;
+				}
+		} else {
+			action_index = action_id;
+		}
+		
+		//2. Go over all DALS.Timeline instances that branch from this timeline at an index greater or equal to the action being removed and set their new .parent_timeline to the end of the present timeline
+		for (let i = 0; i < DALS.Timeline.instances.length; i++) {
+			let local_timeline = DALS.Timeline.instances[i];
+			
+			if (local_timeline.parent_timeline && local_timeline.parent_timeline[0] === this.id)
+				if (local_timeline.parent_timeline[1] >= action_index)
+					local_timeline.parent_timeline[1] = action_index - 1;
+		}
+		
+		//3. Splice all actions at and after the index from the current timeline
+		if (action_index >= 1)
+			for (let i = this.value.length - 1; i >= action_index; i--)
+				this.value.splice(i, 1);
 	}
 	
 	static getTimeline (arg0_timeline_id) {
